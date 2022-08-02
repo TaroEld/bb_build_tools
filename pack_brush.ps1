6c9fca770ad086befcfe606cbfae15e1e82a723c
taroeld <tarob@live.de>
2022-08-02 08:48:26 +0200

    fix: pack_brush, also improve paths

--- pack_brush.ps1
+++ pack_brush.ps1
@@ -1,17 +1,36 @@
 $utilsPath = $PSScriptRoot
 $bbrusherPath = Join-Path $utilsPath \modtools\bbrusher.exe
-
+$modPath = $args[0]
 Set-Location $args[0]
-$folderPath = Join-Path $args[0] "unpacked"
+$folderPath = Join-Path $modPath "unpacked"
+$brushesPath = Join-Path $modPath "brushes"
+$output = 0
 
 if (Test-Path -Path $folderPath)
 {
-  Foreach ($f in Get-ChildItem -Path '.\unpacked')
+  Foreach ($f in Get-ChildItem -Path $folderPath)
   {
-    & $bbrusherPath pack "$f.brush" ".\unpacked\$f"  
-    copy .\$f.brush .\brushes
+    $packResult = & $bbrusherPath pack "$f.brush" "$folderPath\$f"
+    if ($LASTEXITCODE -eq 2)
+    {
+        Write-Output $packResult
+        Write-Output "Failed to write brush $f.brush!"
+        exit -2
+    }
+    if(!(Test-Path -Path $brushesPath))
+    {
+        mkdir $brushesPath
+    }
+    copy $modPath\$f.brush $brushesPath | out-null
+    del $modPath\$f.brush
+    Write-Output "Packed brush $f.brush"
+    $output = 1
   }
 }
-
-
-Set-Location $utilsPath
+$modFoldersPath = Join-Path $modPath ".."
+if (Test-Path -Path $modFoldersPath/gfx)
+{
+  copy -r -force $modFoldersPath/gfx $modPath
+  del -r $modFoldersPath/gfx
+}
+exit 1
