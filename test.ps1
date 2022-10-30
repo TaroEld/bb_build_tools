cb3c32822a58683586879b9e1744f43b887832df
taroeld <tarob@live.de>
2022-10-30 09:26:45 +0100

    fix: remove leftover test file

--- test.ps1
+++
@@ -1,5 +0,0 @@
-$preloadPath = "./template/scripts/!mods_preload/template.nut" 
-$preloadFile = Get-Content "template_preload.nut" 
-$name = "testmod"
-$preloadFile[1] = "        ID = ""$name"""
-$preloadFile
